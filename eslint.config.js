a4251326bc965af0b8622900c7befb3044a7ca5b
Tala Fahed <tala.farahe@gmail.com>
2026-03-12 00:20:20 +0300

    fix Security Issues

--- eslint.config.js
+++ eslint.config.js
@@ -8,4 +8,7 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginSecurity.configs.recommended,
+  {
+    ignores: ["dist", "node_modules"],
+  },
 ]);
